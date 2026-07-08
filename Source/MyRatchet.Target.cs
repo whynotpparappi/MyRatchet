75ee945ed200f2886ede3138ae24c4c94a1bc06d
whynotpparappi <145239764+whynotpparappi@users.noreply.github.com>
2026-07-09 00:48:58 +0900

    5.8 버전업 + gitignore 설정 변경

--- Source/MyRatchet.Target.cs
+++ Source/MyRatchet.Target.cs
@@ -8,8 +8,8 @@ public class MyRatchetTarget : TargetRules
 	public MyRatchetTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("MyRatchet");
 	}
 }
--- Source/MyRatchetEditor.Target.cs
+++ Source/MyRatchetEditor.Target.cs
@@ -8,8 +8,8 @@ public class MyRatchetEditorTarget : TargetRules
 	public MyRatchetEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("MyRatchet");
 	}
 }
